dd080752a548f4f634d53f89fb6bba820b4e3cc3
Pedro Ribeiro <kypedroribeiro@gmail.com>
2022-01-11 17:30:32 -0300

    Jogo da forca começando a funcionar.

--- forca.py
+++ forca.py
@@ -1,9 +1,26 @@
-
-
 def jogar():
     print("***************************")
     print("Bem vindo ao jogo da forca!")
     print("***************************")
+
+    palavra_secreta = "melancia"
+    enforcou = False
+    acertou = False
+
+    #Enquanto não False (True) e não False (True).
+    while(not enforcou and not acertou):
+
+        chute = input ("Qual letra?")
+        chute = chute.strip()
+        index = 0
+
+        for letra in palavra_secreta:
+
+            if(chute.upper() == letra.upper()):
+                print("Você encontrou a letra {} na posição {}".format (letra.upper(), index))
+                index = index + 1
+
+        print("jogando...")
 
     print("Fim do jogo")
 
