b2de3400c236652891934f53d1701a6d3b8e37aa
Pedro Ribeiro <kypedroribeiro@gmail.com>
2022-01-14 12:14:26 -0300

    Praticando exercícios

--- exercicios.py
+++ exercicios.py
@@ -67,3 +67,10 @@
 #else:
 #    print("Usuário não identificado!")
 
+
+#frutas = ['Banana', 'Maca', 'Pera', 'Uva', 'Melancia', 'Jamelão']
+#fruta_pedida = input('Qual é a fruta que deseja consultar ?')
+#if(fruta_pedida in frutas):
+#    print ('Sim, temos a fruta.')
+#else:
+#    print ('Não temos a fruta.')
