5e748472c101f425d867faf6ec0ab7fd4d14d41b
Pedro Ribeiro <kypedroribeiro@gmail.com>
2022-01-14 16:52:51 -0300

    Finalizando o jogo da forca

--- forca.py
+++ forca.py
@@ -3,25 +3,38 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("***************************")
 
-    palavra_secreta = "melancia"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
 
-    #Enquanto não False (True) e não False (True).
-    while(not enforcou and not acertou):
+    print(letras_acertadas)
+    #enquanto(True)
+    while (not enforcou and not acertou):
 
-        chute = input ("Qual letra?")
-        chute = chute.strip()
-        index = 0
+        chute = input("Qual letra?")
+        chute = chute.strip().upper()
 
-        for letra in palavra_secreta:
-
-            if(chute.upper() == letra.upper()):
-                print("Você encontrou a letra {} na posição {}".format (letra.upper(), index))
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
                 index = index + 1
+        else:
+            erros = erros + 1
+            print ("Ops, você errou! Faltam {} tentativas, use-as com cuidado...".format(6-erros))
 
-        print("jogando...")
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
+    if(acertou):
+        print("Parabéns, você ganhou!!")
+    else:
+        print("Infelizmente você perdeu :(")
     print("Fim do jogo")
 
 if (__name__ == "__main__"):
