6c3e7f467eb86f1dab3ba310a3dfc1c8c13d5eda
Pedro Ribeiro <kypedroribeiro@gmail.com>
2022-02-07 17:43:59 -0300

    Atributos privados e encapsulamento

--- conta.py
+++ conta.py
@@ -4,7 +4,20 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}.".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saque(self, valor):
+        self.__saldo -= valor
+
+    def transferencia(self, valor, destinatario):
+        self.saque(valor)
+        destinatario.deposita(valor)
